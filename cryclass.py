7c4f03d6cab50bcdd607d3a0896a5e061245bec1
Paula Frias <pfrias.arroyo@gmail.com>
2019-04-06 09:22:08 +0200

    [Updated] Improve code version Paula

--- cryclass.py
+++ cryclass.py
@@ -1,16 +1,70 @@
 from pathlib import Path
 import csv
+import urllib.request
+from bs4 import BeautifulSoup
+import datetime
+
 class Cryptoclass():
     cols = []
     rows = []
     time = ""
 
-    # def __init__(self, columns):
-    #     for c in columns:
-    #         self.cols.append(c)
-        
-        # for r in rows:
-        #     print(r)
+    def __init__(self):
+        self.page = "https://coinmarketcap.com/"
+
+    def scraping(self):
+        response = urllib.request.urlopen(self.page)
+        html = response.read()
+        bs = BeautifulSoup(html, 'html.parser')
+        tab = bs.find('table')
+        self.time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M")
+        # COLUMNS
+        head = tab.findAll('th')
+        header = []
+        for h in range(0,7):
+                if h != 0:
+                        header.append(head[h].text)
+                else:
+                        header.append("");
+        header.append("timestamp")
+        self.setColumns(header)
+        # ROWS
+        body = []
+        rest = []
+        for row in tab.findAll('tr'):
+            fila = ""    
+            for c in row.findAll('td'):  
+                fila += c.text
+            rest.append(fila)
+
+        resto = []
+        for i in range(1,len(rest)):
+            resto.append(rest[i].split())
+
+        fila = []
+        for r in range(0,len(resto)):
+                for j in range(0,9):
+                        if j == 2 and type(resto[r][j+1]) is str and not (any(char.isdigit() for char in resto[r][j+1])):
+                                fila.append(resto[r][j] + " " + resto[r][j+1])
+                        elif (j == 3) and (any(char.isdigit() for char in resto[r][j])):
+                                fila.append(resto[r][j]) 
+                        elif j == 6 and not (any(char.isdigit() for char in resto[r][3])):
+                                fila.append(resto[r][j])
+                        elif j == 6 and (any(char.isdigit() for char in resto[r][3])):
+                                fila.append(resto[r][j] + " " + resto[r][j+1])                
+                        elif (j == 7) and not (any(char.isdigit() for char in resto[r][3])):
+                                fila.append(resto[r][j] + " " + resto[r][j+1])
+                        elif (j == 8) and not (any(char.isdigit() for char in resto[r][3])):
+                                fila.append(resto[r][j+1])
+                        elif (j == 8) and (any(char.isdigit() for char in resto[r][3])):
+                                fila.append(resto[r][j])
+                        elif (j != 3) and (j != 7) and (j != 8) and (j != 1):
+                                fila.append(resto[r][j])
+                fila.append(self.time)
+                body.append(fila)  
+                fila = []
+
+        self.setRows(body)
 
     def setColumns(self, columns):
         self.cols = columns
